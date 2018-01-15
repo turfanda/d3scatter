5eab967d02a3c12174d0c17aff0cac8c99e46ee1
Glitch (hello-website) <none>
2018-01-15 06:10:12 +0000

    scatter start

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('hi');
+var d3;
+      
+var svg = d3.select("svg");
+      var margin = 100;
+      var width = svg.attr("width") - margin;
+      var height = svg.attr("height") - margin;
+    
+      var xScale = d3.scaleTime().range ([0, width]);
+      var yScale = d3.scaleLinear().range ([height, 0]);
+
+      var g = svg.append("g").attr("transform", "translate(" + 50+"," + 50+ ")");
+
+d3.json("https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/cyclist-data.json",function(err,data){
+
+   if(err)
+          throw err;
+  
+console.log(data);
+
+});
